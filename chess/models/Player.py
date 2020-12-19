12dfa94d9ee2d1a1a58376b2ba0bd7830c90ebf8
Thierry Chappuis <tchappui@users.noreply.github.com>
2020-12-19 09:33:09 +0100

    Update Player.py

--- chess/models/Player.py
+++ chess/models/Player.py
@@ -1,6 +1,10 @@
+"""docstring documentant le module."""
+
 class Player:
+    """Docstring décrivant la classe."""
     
     def __init__(self, lastname, firstname, birth_date, sex, ranking):
+        """Docstring pour documenter l'initialisation."""
         self.lastname = lastname
         self.firstname = firstname
         self.birth_date = birth_date
